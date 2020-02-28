bcc6338679fe12e4bf5c5bc6e228c05a8e2c3d21
Jean-Philippe Eisenbarth <jean-philippe.eisenbarth@loria.fr>
2020-02-28 12:48:38 +0100

    add new stats + some update

--- sns.py
+++ sns.py
@@ -6,8 +6,70 @@ import json
 import csv
 import matplotlib.pyplot as plt
 import random
+import pytz
+import datetime
+import bisect
+import statistics
 from collections import Counter, OrderedDict
 from enum import Enum
+from timeit import default_timer as timer
+
+
+def geo_distribution_per_hour(json_dir: str):
+    europe = (-25., 45.)
+    america = (-135., -25.0001)
+    asia = (45.0001, 155.)
+    # other1 = (155.0001, 180.)
+    # other2 = (-179.9999, -135.0001)
+    result = {}
+    cnt = Counter()
+    local = pytz.timezone("Europe/Paris")
+    for ind, filename in enumerate(os.listdir(json_dir)):
+        naive_time = datetime.datetime.strptime(os.path.splitext(filename)[0],
+                                                "%Y%m%d-%H:%M:%S")
+        utc_time = local.localize(naive_time, is_dst=None).\
+            astimezone(pytz.utc).strftime("%H:%M:%S")
+        with open(os.path.join(json_dir, filename), 'r') as f:
+            nodes = json.load(f)
+        for node_info in nodes:
+            longitude = float(node_info[11])
+            if europe[0] <= longitude <= europe[1]:
+                cnt['europe'] += 1
+            elif america[0] <= longitude <= america[1]:
+                cnt['america'] += 1
+            elif asia[0] <= longitude <= asia[1]:
+                cnt['asia'] += 1
+            else:
+                if ind == 15:
+                    print(f"Not in europe, america, or asia : {longitude},"
+                          f"{node_info[10]}, {node_info[8]}, {node_info[9]}")
+                cnt['other'] += 1
+        result[utc_time] = cnt
+        cnt = Counter()
+    df = pd.DataFrame.from_dict(result, orient='index')
+    print(df)
+    fig, ax = plt.subplots()
+    sns.lineplot(data=df, ax=ax)
+    ax.set(xlabel='Time (UTC)', ylabel='Number of nodes')
+    for ind, label in enumerate(ax.get_xticklabels()):
+        if ind % 15 == 0:  # every 15th label is kept
+            label.set_visible(True)
+        else:
+            label.set_visible(False)
+    fig.autofmt_xdate()
+    plt.show()
+
+
+def distinct_ip(export_json_dirs: str):
+    all_nodes_set = set()
+    for export_json_dir in export_json_dirs:
+        for filename in os.listdir(export_json_dir):
+            path = os.path.join(export_json_dir, filename)
+            with open(path, 'r') as f:
+                nodes = json.load(f)
+            for node_info in nodes:
+                all_nodes_set.add(f'{node_info[0]}-{node_info[1]}-{node_info[5]}')
+    print(f"Distinct IP in all the data files from the dir : {len(all_nodes_set)}")
 
 
 class ChurnPeriod(Enum):
@@ -16,25 +78,14 @@ class ChurnPeriod(Enum):
     TWOHOUR = 30
     FOURHOUR = 60
     EIGHTHOUR = 120
-    ONEDAY = 500
-
-
-def distinct_ip(json_dir: str):
-    files = os.listdir(json_dir)
-    all_nodes_set = set()
-    for filename in files:
-        with open(os.path.join(json_dir, filename), 'r') as f:
-            nodes = json.load(f)
-        for node_info in nodes:
-            all_nodes_set.add(f'{node_info[0]}-{node_info[1]}-{node_info[5]}')
-    print(f"Distinct IP in all the data files from the dir : {len(all_nodes_set)}")
+    ONEDAY = 360
 
 
 def churn(json_dir: str, period: ChurnPeriod):
     files = sorted(os.listdir(json_dir), reverse=True)
-    # print(len(files))
+    print(len(files))
     files = files[:-(len(files) % period.value)] if len(files) % period.value != 0 else files
-    # print(len(files))
+    print(len(files))
     files_number = period.value
     nodes_sets = []
     nodes_set = set()
@@ -109,32 +160,29 @@ def addr_per_node(csv_file: str, export_json_file: str):
 
     with open(export_json_file, 'r') as f:
         data_json = json.load(f)
-    rows_list = []
+    result = {}
+    i = 1
+    # rows_list = []
     for crawl_node in data.values:
         for exported_node in data_json:
-            exported_node_formatted = f"{exported_node[0]}-{exported_node[1]}-\
-                                        {exported_node[5]}"
+            exported_node_formatted = (
+                f"{exported_node[0]}-{exported_node[1]}-{exported_node[5]}"
+            )
             if crawl_node[1] == exported_node_formatted:
-                rows_list.append(pd.DataFrame([crawl_node],
-                                 columns=["node_index", "node",
-                                 "number of ADDR returned"]))
+                result[i] = crawl_node[2]
+                i += 1
                 break
-    df = pd.concat(rows_list, ignore_index=True)
-
+    df = pd.DataFrame.from_dict(result, orient='index', columns=[None])
     i = 0
     j = 0
     for node in df.values:
-        i += 1 if node[2] == -1 else 0
-        j += 1 if node[2] == 0 else 0
-    print(df.index)
-    print(f"In crawl csv file filtered by export json file, size : {len(df.index)}, number of -1 : {i}, 0 : {j}")
+        i += 1 if node[0] == -1 else 0
+        j += 1 if node[0] == 0 else 0
+    print(f"In crawl csv file filtered by export json file, size : {len(df.index)},"
+          f"number of -1 : {i}, 0 : {j}")
 
-    # ax = sns.relplot(x=data.index, y="number of ADDR returned", edgecolor='none', data=data)
-    # ax.set(xlabel='node index', ylabel='number of ADDR')
-    # plt.show()
-
-    ax = sns.relplot(x=df.index, y="number of ADDR returned", edgecolor='none', data=df)
-    ax.set(xlabel='node index', ylabel='number of ADDR')
+    ax = sns.relplot(edgecolor='none', data=df)
+    ax.set(xlabel='Node index', ylabel='Number of addresses')
     plt.show()
 
 
@@ -162,23 +210,45 @@ def up_nodes_per_sec(csv_files: list):
 
     # plt.show()
 
-    data = pd.read_csv(csv_files[0], names=["timeline", os.path.splitext(csv_files[0])[0].split('_')[-1]])
+    data = pd.read_csv(csv_files[0], names=[os.path.splitext(csv_files[0])[0].split('_')[-1]])
     for csv_file in csv_files[1:]:
-        data_to_add = pd.read_csv(csv_file, names=["timeline", os.path.splitext(csv_file)[0].split('_')[-1]])
-        data = pd.merge(data, data_to_add, how='outer')
-    data = pd.melt(data, ['timeline'], var_name='date of crawl', value_name='up nodes')
-    ax = sns.lineplot(x="timeline", y="up nodes", hue="date of crawl", data=data)
+        data_to_add = pd.read_csv(csv_file, names=[os.path.splitext(csv_file)[0].split('_')[-1]])
+        data = data.join(data_to_add, how='outer')
+    ax = sns.lineplot(data=data)
     ax.set(xlabel='time (s)', ylabel='number of nodes')
     plt.show()
+
+
+def number_of_nodes(export_json_dirs):
+    number_of_nodes = []
+    minimum = (None, None)
+    maximum = (None, None)
+    for export_json_dir in export_json_dirs:
+        for filename in os.listdir(export_json_dir):
+            path = os.path.join(export_json_dir, filename)
+            with open(path, 'r') as f:
+                nodes = json.load(f)
+            number = len(nodes)
+            bisect.insort(number_of_nodes, number)
+            if minimum[0] is None or minimum[0] > number:
+                minimum = (number, path)
+            if maximum[0] is None or maximum[0] < number:
+                maximum = (number, path)
+            # number_of_nodes.append(len(nodes))
+    print(f"Min : {minimum}\nMax : {maximum}\n"
+          f"Moy : {statistics.mean(number_of_nodes)}\n"
+          f"Median : {statistics.median(number_of_nodes)}")
 
 
 def main(argv):
     sns.set()
     # up_nodes_per_sec(argv[1:])
     # addr_per_node(argv[1], argv[2])
-    client_distribution(argv[1], argv[2], int(argv[3]), int(argv[4]), argv[5])
-    # churn(argv[1], ChurnPeriod.THIRTYMIN)
-    # distinct_ip(argv[1])
+    # client_distribution(argv[1], argv[2], int(argv[3]), int(argv[4]), argv[5])
+    churn(argv[1], ChurnPeriod.EIGHTHOUR)
+    # distinct_ip(argv[1:])
+    # geo_distribution_per_hour(argv[1])
+    # number_of_nodes(argv[1:])
 
 
 if __name__ == "__main__":
